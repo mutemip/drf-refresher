d6bbc24926e58f8f4c1eb4b50b65bf8b344ab1f3
mutemip <paulmutemi2030@gmail.com>
2024-01-03 12:28:37 +0300

    restart

--- bookAPI/wsgi.py
+++ bookAPI/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for profiles project.
+WSGI config for bookAPI project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profiles.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookAPI.settings')
 
 application = get_wsgi_application()
--- bookAPP/apps.py
+++ bookAPP/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProfilesappConfig(AppConfig):
+class BookappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'profilesApp'
+    name = 'bookAPP'
