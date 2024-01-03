8817c190de003fe4fee99e30a955ec541d3f4897
mutemip <paulmutemi2030@gmail.com>
2024-01-03 17:00:56 +0300

    added serializers

---
+++ bookAPP/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from bookAPP.models import Review, Ebook
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+class EbookSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    class Meta:
+        model = Ebook
+        fields = "__all__"
